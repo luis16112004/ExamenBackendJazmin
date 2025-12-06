d010d725032c3115251fa44683680e359e0fc443
luisc <luisc@LUIS16>
2025-12-05 20:54:40 -0600

    Forzando actualizen Railway

--- ExamenBackendJazmin/routes/web.php
+++ ExamenBackendJazmin/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/limpiar-todo', function() {
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    return "¡Caché borrado! Ahora intenta usar la app de nuevo.";
+});
